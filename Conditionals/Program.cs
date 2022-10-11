ebd9a53316c2bef4e1e67f834c0d5568ffc9127d
N1sgamm <N1sgamm@gmail.com>
2022-10-11 08:57:25 -0300

    started project
    copied everything so far

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,21 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Conditionals");
+            Console.WriteLine();
+
+            int score;
+            string gamertag;
+
+            score = 0;
+            gamertag = "N1sgamm"; //pretend
+            if(gamertag == "N1sgamm")
+            {
+                score = 1000000; //cheating
+            }
+            Console.WriteLine("Score: " + score);
+
+            Console.ReadKey(true);
         }
     }
 }
